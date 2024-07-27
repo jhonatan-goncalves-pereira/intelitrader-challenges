6ff22e23e029aac7ffe0ccb18fa0541c03b7ba59
Jhonatan Gonçalves <jhonatangoncalves2002@gmail.com>
2024-07-27 15:19:57 -0300

    :sparkles: feat: Implementação do desafio de distância mínima

--- menor_distancia_array/Program.cs
+++ menor_distancia_array/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array1 = {-1, 5, 13, 8, 17, 20, 25, 11, 4, 3};
+        int[] array2 = {26, 6, 15, 10, 7, 2, 19, 22, 9, 14};
+
+        int minDistance = int.MaxValue;
+
+        foreach (int num1 in array1)
+        {
+            foreach (int num2 in array2)
+            {
+                int distance = Math.Abs(num1 - num2);
+                if (distance < minDistance)
+                {
+                    minDistance = distance;
+                }
+            }
+        }
+
+        Console.WriteLine("A distância mínima é: " + minDistance);
+    }
+}
